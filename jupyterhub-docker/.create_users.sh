2c25d79b1c59183d3bc09c349eb2bde3adfe2e55
Sjoerd de Vries <sjdv1982@gmail.com>
2019-06-03 09:05:35 +0200

    JupyterHub container: fix home user owner issue

--- jupyterhub-docker/.create_users.sh
+++ jupyterhub-docker/.create_users.sh
@@ -4,4 +4,5 @@ for i in `seq 20`; do
   adduser --quiet --disabled-password --shell /bin/bash --home /home/$user --gecos "User" $user
   echo $user":jupyter2019" | chpasswd
   cp -r /course /home/$user/course
+  chown -R $user /home/$user
 done
